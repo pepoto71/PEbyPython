bfc0641a589c0144f41eb7ced01ba830e1f67ffc
pepoto71 <pepoto71@gmail.com>
2015-02-28 17:10:51 +0200

    add function

--- count_distinct_elements.py
+++ count_distinct_elements.py
@@ -18,3 +18,30 @@ def count(sequence,item):
 item={}
 arr=[1,2,[],{},3,2,2,2,2,3,0]
 print 'item %s ,exist in sequence %d times' %(str(item),count(arr,{}))
+
+#############################################
+
+def remove_duplicates(lst):
+    '''
+    Remove duplicates in linear time O(n)
+    '''
+    temp={}
+    result=[]
+    for item in lst:
+        if str(item) in temp:
+            continue
+        else:
+            temp[str(item)]=True
+            result.append(item)        
+    return result
+    
+lst=[1,2,2,2,1,{},{},[],3,-4,-4,-4,0,0,'qwerty','qwerty']
+print remove_duplicates(lst)
+
+def median(lst):
+    lst.sort()
+    if len(lst)%2==1:
+     return lst[len(lst)/2]
+    else:
+        return (lst[len(lst)/2-1]+lst[len(lst)/2])/2.0
+print median([4,5,5,4])
