2d4a5c93771df0546db4017bd21fd93b71c2a823
pepoto71 <pepoto71@gmail.com>
2015-02-28 13:28:00 +0200

    add new file solving replacing problem in sequence in linear time

---
+++ beginer_guide.py
@@ -0,0 +1,37 @@
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+weights={
+    'homework':0.1,
+    'quizzes':0.3,
+    'tests':0.6
+    }
+
+# Add your function below!
+def average(nums):
+    return float(sum(nums))/len(nums)
+    
+def get_average(student):
+    total=0
+    for item in student:
+        if item!='name':            
+            total +=average(student[item])*weights[item]
+    return total           
+print get_average(alice)
+
---
+++ censor.py
@@ -0,0 +1,31 @@
+def censor(text,word):
+    '''
+    Replace some sequence from given text 
+    with */using state mashines/
+    '''
+    states=len(word)
+    current_state=0
+    temp_buffer=''
+    builder=''
+    for char in text:        
+        if char=='.' or char==',' or char=='!':
+            builder +=char
+            continue
+        else:
+            if char.lower==word[current_state].lower:
+                current_state +=1
+            else:
+                current_state=0
+                builder +=temp_buffer
+                temp_buffer=''
+            if current_state==0:
+                builder +=char
+            else:
+                temp_buffer +=char
+            if current_state==states:
+                builder +='*'*states
+                temp_buffer=''
+                current_state=0
+    return builder
+            
+print censor('Hi i am new for this amazing pyhon programing language. This is my new chalange.','a')
