dff76a7ece342a29cc357ec33442e8f04bd9bb24
Petar Nenov <petar_nenov@abv.bg>
2015-04-04 07:12:13 +0300

    better one

--- censor.py
+++ censor.py
@@ -1,31 +1,41 @@
+text='Hi i am new for this amazing pyhon programing language. This is my new chalange.'
+words=['new','a','hi','hi i am']
+
 def censor(text,word):
     '''
     Replace some sequence from given text 
-    with */using state mashines/ in linear time O(n)
+    */using state mashines/ in linear time O(n)
     '''
     states=len(word)
     current_state=0
     temp_buffer=''
     builder=''
-    for char in text:        
-        if char=='.' or char==',' or char=='!':
+    signs=('.',',','!','?',';',':')#add more punctuation
+    for char in text:         
+        if char in signs:
             builder +=char
             continue
-        else:
-            if char.lower==word[current_state].lower:
+        else:            
+            if char.lower()==word[current_state].lower():
                 current_state +=1
+                temp_buffer +=char
             else:
                 current_state=0
                 builder +=temp_buffer
-                temp_buffer=''
-            if current_state==0:
-                builder +=char
-            else:
-                temp_buffer +=char
+                temp_buffer=''            
+                builder +=char               
             if current_state==states:
                 builder +='*'*states
                 temp_buffer=''
-                current_state=0
+                current_state=0        
     return builder
-            
-print censor('Hi i am new for this amazing pyhon programing language. This is my new chalange.','a')
+
+def test_cases(text,words):
+    print "-=Test cases=-"
+    for word in words:
+        print '='*len(text)    
+        print "Censored word: %s" %word
+        print text
+        print censor(text,word)
+
+test_cases(text,words)       
